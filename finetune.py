34e2cabf25b82a78f2a2096f186f2e316b830c74
Henil Panchal <henilp105@gmail.com>
2024-05-05 13:00:48 +0530

    f16

--- finetune.py
+++ finetune.py
@@ -64,7 +64,7 @@ def main(args):
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_compute_dtype=torch.float16,
     )
     lora_config = LoraConfig(
         r=8,
